f39a2710fbdce50310bd673ad391b6641599a70e
danpolus <danpol@gmail.com>
2022-04-14 12:54:55 +0300

    support repeated cross-validation
    +add several options for features&classifier

--- src/bci4als/ml_model.py
+++ src/bci4als/ml_model.py
@@ -4,10 +4,7 @@ import pandas as pd
 from src.bci4als.eeg import EEG
 import numpy as np
 from mne.channels import make_standard_montage
-from mne.decoding import CSP
 from nptyping import NDArray
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.pipeline import Pipeline
 from sklearn import metrics
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -74,21 +71,69 @@ class MLModel:
         # Apply band-pass filter
         epochs.filter(l_freq=self.filt_l_freq, h_freq=self.filt_h_freq, skip_by_annotation='edge', pad='edge', verbose=False)
 
-        # Assemble a classifier
+        #CSP + LDA classifier
+        from mne.decoding import CSP
+        from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+        from sklearn.pipeline import Pipeline
         lda = LinearDiscriminantAnalysis()
         csp = CSP(n_components=6, reg=None, log=True, norm_trace=False)
+        self.clf = Pipeline([('CSP', csp), ('LDA', lda)])  # Use scikit-learn Pipeline
+        trials_data = epochs.get_data()
 
-        # Use scikit-learn Pipeline
-        self.clf = Pipeline([('CSP', csp), ('LDA', lda)])
+        # #many features + SVM classifier
+        # from sklearn.svm import SVC
+        # from sklearn.preprocessing import StandardScaler
+        # from sklearn.pipeline import make_pipeline
+        # from sklearn.decomposition import PCA
+        # from sklearn.manifold import TSNE
+        # from mne_features import feature_extraction
+        # from mne.decoding import CSP
+        # self.clf = make_pipeline(StandardScaler(), SVC(C=1, kernel='linear')) #regularizarion C >1. greater C for better generalization
+        # # self.clf = SVC(C=1, kernel='linear')
+        # yt = self.labels+self.augmented_labels
+        # trials_data = feature_extraction.extract_features(epochs.get_data(), sfreq, ['pow_freq_bands','kurtosis','rms','hurst_exp','decorr_time','samp_entropy','spect_entropy','spect_slope','hjorth_mobility','hjorth_complexity','teager_kaiser_energy','phase_lock_val','time_corr','spect_corr'], \
+        #             funcs_params={'pow_freq_bands__freq_bands': np.arange(self.filt_l_freq, self.filt_h_freq)})
+        #             # funcs_params={'pow_freq_bands__freq_bands': np.array([0.5, 4., 8., 12., 28., 40.])})
+        # csp = CSP(n_components=6, reg=None, log=True, norm_trace=False)
+        # trials_data = np.append(trials_data, csp.fit_transform(epochs.get_data(), yt), axis=1) #on validation use transform instead of fit_transform
+        # # csp.plot_patterns(epochs.info, ch_type='eeg', show_names=True, units='Patterns (AU)', size=1.5)
+        # # csp.plot_filters(epochs.info, ch_type='eeg', show_names=True, units='Patterns (AU)', size=1.5)
+        # pca = PCA()
+        # scaler = StandardScaler()
+        # trials_data = scaler.fit_transform(trials_data,yt)
+        # Xt = pca.fit_transform(trials_data)
+        # fig = plt.figure()
+        # ax = fig.add_subplot(projection='3d')
+        # ax.scatter(Xt[:,0], Xt[:,1], Xt[:,2], c=yt)
+        # ax.set_xlabel("PC1 variance: {var:.2f}".format(var = pca.explained_variance_ratio_[0]))
+        # ax.set_ylabel("PC2 variance: {var:.2f}".format(var = pca.explained_variance_ratio_[1]))
+        # ax.set_zlabel("PC3 variance: {var:.2f}".format(var = pca.explained_variance_ratio_[2]))
+        # plt.show(block=False)
+        # tsne = TSNE(n_components=2, perplexity=50, learning_rate='auto', init='random') #play with perplexity and init, see http://arxiv.org/abs/2006.05331
+        # Xt = tsne.fit_transform(trials_data)
+        # plt.figure()
+        # plt.scatter(Xt[:,0], Xt[:,1], c=yt)
+        # plt.show(block=False)
+        # #svm descision boundary visualization: https://scikit-learn.org/0.18/auto_examples/svm/plot_iris.html
+
+        # #raw eeg + multi-layer perceptron
+        # from sklearn.neural_network import MLPClassifier
+        # self.clf = MLPClassifier(hidden_layer_sizes=(int(epochs.times.shape[0]/4), int(epochs.times.shape[0]/8), 40, 40, 20), verbose=True)
+        # trials_data = epochs.get_data().reshape((len(epochs),-1))
 
         #cross validation
-        self.cross_valid(epochs)
+        # from sklearn.model_selection import cross_validate, KFold
+        # cv_results = cross_validate(self.clf, trials_data, self.labels, return_train_score=True, cv=KFold(n_splits=5, shuffle=True))
+        self.cross_valid(trials_data, True)
+        crossv_res = {'cv_train':[],'cv_test':[]}
+        for i in range(20):
+            train_acc, val_acc = self.cross_valid(trials_data)
+            crossv_res['cv_train'] += [train_acc]
+            crossv_res['cv_test'] += [val_acc]
+        print('train acc: {0:0.2f}+-{1:0.3f}, val acc: {2:0.2f}+-{3:0.3f}'.format(np.mean(crossv_res['cv_train']), np.std(crossv_res['cv_train']), np.mean(crossv_res['cv_test']), np.std(crossv_res['cv_test'])))
 
         # fit transformer and classifier to data
-        self.clf.fit(epochs.get_data(), self.labels + self.augmented_labels)
-
-        csp.plot_patterns(epochs.info, ch_type='eeg', units='Patterns (AU)', size=1.5)
-        # self.clf.named_steps['CSP'].plot_patterns(epochs.info, ch_type='eeg', units='Patterns (AU)', size=1.5)
+        self.clf.fit(trials_data, self.labels + self.augmented_labels)
 
     def online_predict(self, data: NDArray, eeg: EEG):
         # Prepare the data to MNE functions
@@ -117,7 +162,7 @@ class MLModel:
             com_pred = Commands.idle #pred==2
         return com_pred, pred_prob.max()
 
-    def cross_valid(self, epochs):
+    def cross_valid(self, all_trials, plot_flg=False):
 
         mpl.use('TkAgg')
 
@@ -125,22 +170,21 @@ class MLModel:
         nTrials = len(self.labels)
         foldSize = int(np.ceil(nTrials/nFold))
 
-        all_trials = epochs.get_data()
         all_labels = np.array(self.labels+self.augmented_labels)
 
         trials_inx = np.arange(nTrials)
         augmented_trials_inx = np.arange(len(self.augmented_labels))+nTrials
         np.random.shuffle(trials_inx)
 
-        y_train_join = np.array([])
-        y_val_join = np.array([])
-        pred_val_join = np.array([])
-        pred_train_join = np.array([])
+        y_train_join = np.array([],dtype=int)
+        y_val_join = np.array([],dtype=int)
+        pred_val_join = np.array([],dtype=int)
+        pred_train_join = np.array([],dtype=int)
         for iFold in range(nFold):
             validation_inx = trials_inx[range(foldSize*iFold, min(foldSize*(iFold+1), nTrials))]
             train_inx = np.append(np.setdiff1d(trials_inx, validation_inx, assume_unique=True), augmented_trials_inx)
-            X_train = all_trials[train_inx,:,:]
-            X_val = all_trials[validation_inx,:,:]
+            X_train = all_trials[train_inx]
+            X_val = all_trials[validation_inx]
             y_train = all_labels[train_inx]
             y_val = all_labels[validation_inx]
 
@@ -154,13 +198,18 @@ class MLModel:
             pred_train_join = np.append(pred_train_join,pred_train)
             pred_val_join = np.append(pred_val_join,pred_val)
 
-        print('train accuracy score: {0:0.4f}'.format(metrics.accuracy_score(y_train_join, pred_train_join)))
-        print('validation accuracy score: {0:0.4f}'.format(metrics.accuracy_score(y_val_join, pred_val_join)))
-        cm_train = metrics.confusion_matrix(y_train_join, pred_train_join)
-        cm_val = metrics.confusion_matrix(y_val_join, pred_val_join)
-        disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm_train, display_labels=['right', 'left', 'idle','tongue', 'legs'])
-        disp.plot()
-        plt.show(block=False)
-        disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm_val, display_labels=['right', 'left', 'idle','tongue', 'legs'])
-        disp.plot()
-        plt.show(block=False)
+        train_acc = metrics.accuracy_score(y_train_join, pred_train_join)
+        val_acc = metrics.accuracy_score(y_val_join, pred_val_join)
+        print('train accuracy score: {0:0.2f}'.format(train_acc))
+        print('validation accuracy score: {0:0.2f}'.format(val_acc))
+        if plot_flg:
+            cm_train = metrics.confusion_matrix(y_train_join, pred_train_join)
+            cm_val = metrics.confusion_matrix(y_val_join, pred_val_join)
+            disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm_train, display_labels=['right', 'left', 'idle','tongue', 'legs'])
+            disp.plot()
+            plt.show(block=False)
+            disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm_val, display_labels=['right', 'left', 'idle','tongue', 'legs'])
+            disp.plot()
+            plt.show(block=False)
+
+        return train_acc, val_acc
